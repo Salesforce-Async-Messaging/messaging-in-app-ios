0dbfdb4409680b79cacc8345855c38724ab3899b
aeisses <2683848+aeisses@users.noreply.github.com>
2024-06-11 13:51:11 -0300

    1.6.0 release. (#32)

--- examples/MessagingUIExample/Views/ContentView.swift
+++ examples/MessagingUIExample/Views/ContentView.swift
@@ -4,6 +4,7 @@
 
 import SwiftUI
 import SMIClientUI
+import SMIClientCore
 
 struct ContentView: View {
     @StateObject private var controller = MessagingController()
@@ -16,6 +17,7 @@ struct ContentView: View {
                 Spacer()
                 VStack {
                     if let config = controller.uiConfig {
+//                        NavigationLink(destination: Interface(config, preChatFieldValueProvider: setPreChatValues())) { // Set the pre populated pre-chat
                         NavigationLink(destination: Interface(config)) {
                             Text("Speak with an Agent")
                         }
@@ -31,6 +33,20 @@ struct ContentView: View {
             .padding()
         }
     }
+
+    // Pre set the pre-chat values
+    private func setPreChatValues() -> (([PreChatField]) async throws -> [PreChatField]) { { prechatFields in
+        prechatFields.enumerated().forEach { (index, value) in
+            let currenPrechatName = value.name
+
+            if currenPrechatName == "Name of the Pre-Chat field you would like to set" {
+                prechatFields[index].value = "Set the value for the pre-chat fields"
+                prechatFields[index].isEditable = false // Set it to true to make the pre-chat fields editable, or to false to make them non-editable
+            }
+        }
+
+        return prechatFields
+    }}
 }
 
 struct ContentView_Previews: PreviewProvider {
