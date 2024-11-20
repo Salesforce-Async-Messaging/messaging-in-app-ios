a4e0d39e5fd3bb4b248f6d0261528ef28a5abd99
Nigel Brown <nigelbrown@salesforce.com>
2024-11-20 15:12:23 -0400

    W-17274626: Need to fix the test app so it doesn't break the settings page for previous version when an update is made to the settings

--- examples/Shared/Enums/ConnectionEnvironment.swift
+++ examples/Shared/Enums/ConnectionEnvironment.swift
@@ -21,13 +21,32 @@ enum ConnectionEnvironment: String, EnvironmentSettings {
 extension MIAWConfigurationStore {
     var environments: [String: ConnectionConfigurationModel] {
         get {
-            guard let dictionary = [String: ConnectionConfigurationModel](rawValue: userDefaults.string(forKey: Keys.environments.rawValue) ?? "") else {
+            guard let jsonString = userDefaults.string(forKey: Keys.environments.rawValue),
+                  let jsonData = jsonString.data(using: .utf8) else {
                 return [:]
             }
 
-            return dictionary
+            do {
+                let dictionary = try JSONDecoder().decode([String: ConnectionConfigurationModel].self, from: jsonData)
+                return dictionary
+            } catch {
+                print("Error decoding environments from UserDefaults: \(error)")
+                return [:]
+            }
         }
 
-        set { userDefaults.set(newValue.rawValue, forKey: Keys.environments.rawValue) }
+        set {
+            do {
+                let jsonData = try JSONEncoder().encode(newValue)
+
+                if let jsonString = String(data: jsonData, encoding: .utf8) {
+                    userDefaults.set(jsonString, forKey: Keys.environments.rawValue)
+                } else {
+                    print("Failed to convert JSON data to a string.")
+                }
+            } catch {
+                print("Error encoding environments to UserDefaults: \(error)")
+            }
+        }
     }
 }
--- examples/Shared/Settings/Models/ConnectionConfigurationModel.swift
+++ examples/Shared/Settings/Models/ConnectionConfigurationModel.swift
@@ -16,4 +16,53 @@ struct ConnectionConfigurationModel: Codable {
     var enableEndSessionUI: Bool = true
     var URLDisplayMode: String = UrlDisplayMode.inlineBrowser.rawValue
     var useSSL: Bool = true
+
+    enum CodingKeys: String, CodingKey {
+        case domain
+        case organizationId
+        case developerName
+        case userVerificationRequired
+        case enableAttachmentUI
+        case enableTranscriptUI
+        case useProgressIndicatorForAgents
+        case enableEndSessionUI
+        case URLDisplayMode
+        case useSSL
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        domain = try container.decode(String.self, forKey: .domain)
+        organizationId = try container.decode(String.self, forKey: .organizationId)
+        developerName = try container.decode(String.self, forKey: .developerName)
+        userVerificationRequired = try container.decodeIfPresent(Bool.self, forKey: .userVerificationRequired) ?? false
+        enableAttachmentUI = try container.decodeIfPresent(Bool.self, forKey: .enableAttachmentUI) ?? true
+        enableTranscriptUI = try container.decodeIfPresent(Bool.self, forKey: .enableTranscriptUI) ?? true
+        useProgressIndicatorForAgents = try container.decodeIfPresent(Bool.self, forKey: .useProgressIndicatorForAgents) ?? true
+        enableEndSessionUI = try container.decodeIfPresent(Bool.self, forKey: .enableEndSessionUI) ?? true
+        URLDisplayMode = try container.decodeIfPresent(String.self, forKey: .URLDisplayMode) ?? UrlDisplayMode.inlineBrowser.rawValue
+        useSSL = try container.decodeIfPresent(Bool.self, forKey: .useSSL) ?? true
+    }
+
+    init(domain: String,
+        organizationId: String,
+        developerName: String,
+        userVerificationRequired: Bool = false,
+        enableAttachmentUI: Bool = true,
+        enableTranscriptUI: Bool = true,
+        useProgressIndicatorForAgents: Bool = true,
+        enableEndSessionUI: Bool = true,
+        URLDisplayMode: String = UrlDisplayMode.inlineBrowser.rawValue,
+        useSSL: Bool = true) {
+        self.domain = domain
+        self.organizationId = organizationId
+        self.developerName = developerName
+        self.userVerificationRequired = userVerificationRequired
+        self.enableAttachmentUI = enableAttachmentUI
+        self.enableTranscriptUI = enableTranscriptUI
+        self.useProgressIndicatorForAgents = useProgressIndicatorForAgents
+        self.enableEndSessionUI = enableEndSessionUI
+        self.URLDisplayMode = URLDisplayMode
+        self.useSSL = useSSL
+    }
 }
